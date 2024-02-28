f465699df7798eca9cd97d83f15a71e7b34f13d1
incarik <incarik1@gmail.com>
2024-02-28 10:14:51 +0100

    Examen de programación

---
+++ Assets/Scripts/Player move.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermove : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-speed, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed, 0, 0);
+        }
+         if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, speed, 0);
+        }
+    }
+}
